790bad23242e9f0027f501ee839aa500fea97546
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-03-18 11:02:26 +0700

    Penambahan perulangan untuk pencacah naik

---
+++ file1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int cacah;
+    // perulangan for untuk pencacah naik
+    for(cacah = 0; cacah <= 4; cacah++){
+        cout << "Nilai Pencacah = " << cacah << " Selamat Datang!" << endl;
+    }
+    cout << "Nilai Pencacah = " << cacah;
+    cout << endl;
+    cout << endl;
+}
